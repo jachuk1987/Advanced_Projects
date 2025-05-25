fc8c08209735dc0f7e1b1b120feb19cc1bb87eb7
jeswanth <jachu.k1212@gmail.com>
2025-05-25 06:42:22 +0530

    JobSearch_Platform

--- job-search-app/src/main.tsx
+++ job-search-app/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
