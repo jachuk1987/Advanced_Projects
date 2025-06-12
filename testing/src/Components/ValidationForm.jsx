dfd773149a7a824c30e3e9fd391d8b620f0dea24
jeswanth <jachu.k1212@gmail.com>
2025-06-12 15:01:02 +0530

    Coding

--- testing/src/Components/ValidationForm.jsx
+++ testing/src/Components/ValidationForm.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const ValidationForm = () => {
     const [user, setUser] = useState({
         fName: "",
-        lName: "",
+        age: "",
         email: "",
     });
 
@@ -11,10 +11,10 @@ const ValidationForm = () => {
   return (
     <div>
       <form>
-        <label htmlFor="">First Name</label>
+        <label htmlFor="">Full Name</label>
         <input type="text" name='fName' value={user.fName} /> <br />
-        <label htmlFor="">Last Name</label>
-        <input type="text" name='lName' value={user.lName}/> <br />
+        <label htmlFor="">Age</label>
+        <input type="text" name='lName' value={user.age}/> <br />
         <label htmlFor="">Email</label>
         <input type="email" name='email' value={user.email}/>
       </form>
