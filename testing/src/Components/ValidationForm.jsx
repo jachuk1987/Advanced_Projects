e5b4f9875bb68ec3f1bdb75bb1372a4c1d1fc938
jeswanth <jachu.k1212@gmail.com>
2025-06-13 11:01:08 +0530

    Code_Testing

--- testing/src/Components/ValidationForm.jsx
+++ testing/src/Components/ValidationForm.jsx
@@ -48,7 +48,7 @@ const ValidationForm = () => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(user)
     })
-  }
+  };
 
   const FetchData = async () => {
     const response = await fetch("http://localhost:3000/Jeswanth")
