daeb55af29d9c99712e94a6ca4426d352ea26fe6
jeswanth <jachu.k1212@gmail.com>
2025-06-15 11:28:57 +0530

    Code_Testing

--- testing/src/Components/CheckList.jsx
+++ testing/src/Components/CheckList.jsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+const checklistData = {
+  Electronics: {
+    "Mobile phones": ["iPhone", "Android"],
+    Laptops: ["MacBook", "Surface Pro"]
+  },
+  Books: ["Fiction", "Non-fiction"],
+  Toys: ["Toys"]
+};
 
 const CheckList = () => {
   return (
