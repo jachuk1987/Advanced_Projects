8ee4fd47fa1338fb8dffff21a97748c5f5fb7bc0
jeswanth <jachu.k1212@gmail.com>
2025-06-16 18:05:55 +0530

    Coding_Questions

---
+++ coding-questions/src/Components/4.DomAccessClass.jsx
@@ -0,0 +1,19 @@
+class DomAccessClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef(); // Create ref
+  }
+
+  handleClick = () => {
+    this.inputRef.current.focus(); // Access DOM
+  };
+
+  render() {
+    return (
+      <div>
+        <input ref={this.inputRef} type="text" />
+        <button onClick={this.handleClick}>Focus Input</button>
+      </div>
+    );
+  }
+}
