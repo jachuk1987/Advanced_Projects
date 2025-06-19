779c5b64b00afca1ec189fd8097e25ba9b9204c4
jeswanth <jachu.k1212@gmail.com>
2025-06-19 10:14:40 +0530

    CustomHooks

--- coding-questions/src/Components/37.Dummy.jsx
+++ coding-questions/src/Components/37.Dummy.jsx
@@ -14,3 +14,17 @@ const Dummy = () => {
 }
 
 export default Dummy;
+
+import React, { useState } from 'react'
+
+const CustomHook = () => {
+    const [count, setCount] = useState(0);
+    const Increment = () => { setCount(count + 1) }
+    const Decrement = () => { setCount(count - 1) }
+    const Reset = () => { setCount(0) }
+
+    return [count, Increment, Decrement, Reset]
+
+}
+
+export default CustomHook;
