b1a8beeaa3a9ccb475c0f5eb57c8fa4fdeb3a8e2
jeswanth <jachu.k1212@gmail.com>
2025-06-19 11:53:01 +0530

    ShowAndHide

--- coding-questions/src/App.js
+++ coding-questions/src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 import DisplayDynamicData from './Components/1.DisplayDynamicData';
 import ParentComponent from './Components/2.ParentComponent';
 import ParentCompo from './Components/3.ParentCompo';
-
+import ShowAndHide from './Components/11.ShowAndHide';
 import Main from './30.ContextAPI/Main';
 import Dummy from './Components/37.Dummy';
-import ShowAndHide from './Components/11.Show&Hide';
 
 function App() {
   return (
