5bc169376462bab6363c6b6d4c01e7deaf1fa5a2
jeswanth <jachu.k1212@gmail.com>
2025-06-19 10:20:45 +0530

    CustomHooks

--- coding-questions/src/Components/37.CustomHook.jsx
+++ coding-questions/src/Components/37.CustomHook.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import CustomHook from './37.CustomHook';
+import React, { useState } from 'react'
 
-const Dummy = () => {
-    const [count, Increment, Decrement, Reset] = CustomHook(0)
-  return (
-    <div>
-      <button onClick={Increment}>Increment</button>
-      <button onClick={Decrement}>Decrement</button>
-      <button onClick={Reset}>Reset</button>
-      <p>{count}</p>
-    </div>
-  )
+const CustomHook = () => {
+    const [count, setCount] = useState(0);
+    const Increment = () => { setCount(count + 1) }
+    const Decrement = () => { setCount(count - 1) }
+    const Reset = () => { setCount(0) }
+
+    return [count, Increment, Decrement, Reset]
+
 }
 
-export default Dummy;
+export default CustomHook;
--- coding-questions/src/Components/37.Dummy.jsx
+++ coding-questions/src/Components/37.Dummy.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
+import CustomHook from './37.CustomHook';
 
-const CustomHook = () => {
-    const [count, setCount] = useState(0);
-    const Increment = () => { setCount(count + 1) }
-    const Decrement = () => { setCount(count - 1) }
-    const Reset = () => { setCount(0) }
-
-    return [count, Increment, Decrement, Reset]
-
+const Dummy = () => {
+    const [count, Increment, Decrement, Reset] = CustomHook(0)
+  return (
+    <div>
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
+      <button onClick={Reset}>Reset</button>
+      <p>{count}</p>
+    </div>
+  )
 }
 
-export default CustomHook;
+export default Dummy;
