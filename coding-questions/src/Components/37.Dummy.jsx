ccd91b14471574eacbd8e1b2cb786c39a01a07ba
jeswanth <jachu.k1212@gmail.com>
2025-06-19 10:15:06 +0530

    CustomHooks

--- coding-questions/src/Components/37.Dummy.jsx
+++ coding-questions/src/Components/37.Dummy.jsx
@@ -1,20 +1,3 @@
-import React from 'react'
-import CustomHook from './37.CustomHook';
-
-const Dummy = () => {
-    const [count, Increment, Decrement, Reset] = CustomHook(0)
-  return (
-    <div>
-      <button onClick={Increment}>Increment</button>
-      <button onClick={Decrement}>Decrement</button>
-      <button onClick={Reset}>Reset</button>
-      <p>{count}</p>
-    </div>
-  )
-}
-
-export default Dummy;
-
 import React, { useState } from 'react'
 
 const CustomHook = () => {
