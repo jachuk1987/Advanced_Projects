d41769709fd30bf5074f15872ba2cc5982cf6b4e
jeswanth <jachu.k1212@gmail.com>
2025-06-19 11:52:23 +0530

    ShowAndHide

--- coding-questions/src/App.js
+++ coding-questions/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import DisplayDynamicData from './Components/1.DisplayDynamicData';
 import ParentComponent from './Components/2.ParentComponent';
 import ParentCompo from './Components/3.ParentCompo';
-import ShowHide from './Components/11.Show&Hide';
+
 import Main from './30.ContextAPI/Main';
 import Dummy from './Components/37.Dummy';
+import ShowAndHide from './Components/11.Show&Hide';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <DisplayDynamicData />
       <ParentComponent />
       <ParentCompo />
-      <ShowHide />
+      <ShowAndHide />
       <Main />
       <Dummy />
     </div>
