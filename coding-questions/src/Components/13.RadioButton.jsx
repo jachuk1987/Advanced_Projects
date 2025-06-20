f17c7b1559a47a65fd388d0d9a5ab61846961b3d
jeswanth <jachu.k1212@gmail.com>
2025-06-20 10:53:44 +0530

    RadioButton

--- coding-questions/src/Components/13.RadioButton.jsx
+++ coding-questions/src/Components/13.RadioButton.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RadioButton = () => {
+    const [selectColor, setSelectColor] = useState("");
+    const colors = ["Green" ,"Red", "Blue"];
+
   return (
     <div>
-      
+      <label htmlFor="">Select Color</label>
+      {colors.map((val, index) => (
+        <label key={index} style={{marginRight:"10px"}}>
+            <input type="radio" name='color' value={val} checked={selectColor === val} onChange={(e)=>{setSelectColor(e.target.value)}} />
+            {val}
+        </label>
+      ))}
+
+      <p>Selected: {selectColor}</p>
     </div>
   )
 }
