0b23ec588522f31b9b92be8e8ac553d1cbf3bfcb
jeswanth <jachu.k1212@gmail.com>
2025-06-21 11:45:17 +0530

    EventHandler

--- coding-questions/src/Components/ControlledUsers.jsx
+++ coding-questions/src/Components/ControlledUsers.jsx
@@ -2,21 +2,29 @@ import React, { useState } from 'react'
 
 const ControlledUsers = () => {
   const [user, setUser] = useState({
-    name:"",
-    age:"",
-    gender:"",
-    mobile:""
-  })
+    name: "",
+    age: "",
+    gender: "",
+    mobile: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setUser((preview) => ({
+      ...preview, [name]: value 
+    }))
+  }
   return (
     <div>
       <label htmlFor="">Name</label>
-      <input type="text" name='name' value={user.name}/>
+      <input type="text" name='name' value={user.name} onChange={handleChange} />
       <label htmlFor="">Age</label>
-      <input type="text" name='age' value={user.age}/>
+      <input type="text" name='age' value={user.age} onChange={handleChange}/>
       <label htmlFor="">Gender</label>
-      <input type="text" name='gender' value={user.gender}/>
+      <input type="text" name='gender' value={user.gender} onChange={handleChange}/>
       <label htmlFor="">Mobile</label>
-      <input type="text" name='mobile'value={user.mobile}/>
+      <input type="text" name='mobile' value={user.mobile} onChange={handleChange}/>
     </div>
   )
 }
