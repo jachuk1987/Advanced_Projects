d6b82ef6741ff1c75c60e33eb555d6862de282a6
jeswanth <jachu.k1212@gmail.com>
2025-06-21 11:53:45 +0530

    SubmitUsers

--- coding-questions/src/Components/ControlledUsers.jsx
+++ coding-questions/src/Components/ControlledUsers.jsx
@@ -12,19 +12,26 @@ const ControlledUsers = () => {
     const { name, value } = e.target
 
     setUser((preview) => ({
-      ...preview, [name]: value 
+      ...preview, [name]: value
     }))
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(user)
+  }
   return (
-    <div>
+    <div style={{ padding: "2rem", maxWidth: "400px", margin: "auto" }}>
       <label htmlFor="">Name</label>
-      <input type="text" name='name' value={user.name} onChange={handleChange} />
+      <input type="text" name='name' value={user.name} onChange={handleChange} /> <br />
       <label htmlFor="">Age</label>
-      <input type="text" name='age' value={user.age} onChange={handleChange}/>
+      <input type="text" name='age' value={user.age} onChange={handleChange} /><br />
       <label htmlFor="">Gender</label>
-      <input type="text" name='gender' value={user.gender} onChange={handleChange}/>
+      <input type="text" name='gender' value={user.gender} onChange={handleChange} /> <br />
       <label htmlFor="">Mobile</label>
-      <input type="text" name='mobile' value={user.mobile} onChange={handleChange}/>
+      <input type="text" name='mobile' value={user.mobile} onChange={handleChange} />  <br />
+
+      <button type='button' onClick={handleSubmit}>Submit</button>
     </div>
   )
 }
