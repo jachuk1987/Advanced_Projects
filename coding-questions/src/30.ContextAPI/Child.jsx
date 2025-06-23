e2cd63292b5761ceebbb1f9fd324c26e7ccdd818
jeswanth <jachu.k1212@gmail.com>
2025-06-23 11:05:03 +0530

    ContexAPI

--- coding-questions/src/30.ContextAPI/Child.jsx
+++ coding-questions/src/30.ContextAPI/Child.jsx
@@ -5,7 +5,7 @@ const Child = () => {
   return (
     <>
     <h6>I am from Child</h6>
-    <div style={{width: "600px", height: "300px", border: "2px solid blue" , margin: "auto"}}>
+    <div style={{width: "600px", height: "300px", border: "2px solid blue" , margin: "auto", backgroundColor:"yellowgreen"}}>
       <GrandChild />
     </div>
     </>
