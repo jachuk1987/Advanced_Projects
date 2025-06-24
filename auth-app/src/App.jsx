4ab8eb2cc4ae52d1844f0d5f07be0705ca15d0fa
jeswanth <jachu.k1212@gmail.com>
2025-06-24 12:53:17 +0530

    Lazy_Loading

--- auth-app/src/App.jsx
+++ auth-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+// import "./App.css";
 import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 import { lazy } from "react";
 import { Suspense } from "react";
