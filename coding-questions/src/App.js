1ee1b7f0def6697a93f6c057e381a48d1b868c17
jeswanth <jachu.k1212@gmail.com>
2025-06-26 07:37:48 +0530

    ArrayOfObjects

--- coding-questions/src/App.js
+++ coding-questions/src/App.js
@@ -7,14 +7,14 @@ import ControlledUsers from './Components/ControlledUsers';
 import DisplayDynamicData from './Components/1.DisplayDynamicData';
 import ParentComponent from './Components/2.ParentComponent';
 import ParentCompo from './Components/3.ParentCompo';
+import ArrayOfArray from './Components/8.ArrayofArray';
+import ArrayOfObjects from './Components/8.ArrayOfObjects';
 import ShowAndHide from './Components/11.ShowAndHide';
 import RadioButton from './Components/13.RadioButton';
 import Main from './30.ContextAPI/Main';
 import Dummy from './Components/37.Dummy';
-import ArrayOfArray from './Components/8.ArrayofArray';
 import SearchInput from './Components/19.SearchFilter';
 import PaginationComponents from './Components/40.Pagination';
-import ArrayOfObjects from './Components/8.ArrayOfObjects';
 
 function App() {
   return (
@@ -26,7 +26,7 @@ function App() {
       <ParentComponent />
       {/* <ParentCompo /> */}
       <ArrayOfArray />
-      <ArrayOfObjects
+      <ArrayOfObjects />
       <ShowAndHide />
       <RadioButton />
       <SearchInput />
