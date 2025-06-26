50146b2542ac6195985a58afd621f9040936cf0a
jeswanth <jachu.k1212@gmail.com>
2025-06-26 07:33:39 +0530

    ArrayOfObjects

--- coding-questions/src/Components/8.ArrayOfObjects.jsx
+++ coding-questions/src/Components/8.ArrayOfObjects.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ArrayOfObjects = () => {
+    const optionsListed = [
+    { id: 1, label: "JavaScript" },
+    { id: 2, label: "Python" },
+    { id: 3, label: "Java" }
+  ];
+
+  const [selected, setSelected] = useState("");
+
   return (
-    <div>
-      
-    </div>
+    <>
+    <h3>Select Language</h3>
+    <select value={selected} onChange={(e)=>{setSelected(e.target.value)}}>
+        <option value="">Select</option>
+        {optionsListed.map((opt)=>(
+            <option value={opt.label} key={opt.id}>{opt.label}</option>
+        ))}
+    </select>
+    <p>You have selected: {selected}</p>
+    </>
   )
-};
+}
 
 export default ArrayOfObjects;
