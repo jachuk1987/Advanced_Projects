e318bc17bc18d90afb473a0ad0a4864b2de224df
jeswanth <jachu.k1212@gmail.com>
2025-06-28 12:05:38 +0530

    Mirror_Input

--- coding-questions/src/Components/7.Mirror_Input.jsx
+++ coding-questions/src/Components/7.Mirror_Input.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Mirror_Input = () => {
+const MirrorTextbox = () => {
+  const [text, setText] = useState('');
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div style={{ padding: '1rem' }}>
+      <h3>Mirror Input</h3>
 
-export default Mirror_Input;
+      <input
+        type="text"
+        placeholder="Enter text here"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        style={{ marginRight: '1rem' }}
+      />
+
+      <input
+        type="text"
+        placeholder="Mirrored here"
+        value={text}
+        readOnly // Optional: makes second box non-editable
+      />
+    </div>
+  );
+};
+
+export default MirrorTextbox;
