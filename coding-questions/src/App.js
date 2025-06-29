3d4767b13856561633e768fdedcb50ffbe2ba7a7
jeswanth <jachu.k1212@gmail.com>
2025-06-29 10:04:35 +0530

    DisplayObjects

--- coding-questions/src/App.js
+++ coding-questions/src/App.js
@@ -22,7 +22,6 @@ import CounterWithReducer from './Components/21.CounterUseReducer';
 import Main from './30.ContextAPI/Main';
 import Dummy from './Components/37.Dummy';
 import PaginationComponents from './Components/40.Pagination';
-import DisplayObject from './Components/15.DisplayObjects';
 
 function App() {
   return (
