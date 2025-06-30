933bbb77e5cc36e4d404c439c9cdce6567a52a28
jeswanth <jachu.k1212@gmail.com>
2025-06-30 08:13:15 +0530

    ReRender

--- coding-questions/src/Components/16.ReRender.jsx
+++ coding-questions/src/Components/16.ReRender.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const 16.ReRender = () => {
+const ReRender = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const 16.ReRender = () => {
   )
 }
 
-export default 16.ReRender
+export default ReRender
